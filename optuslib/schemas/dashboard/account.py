1cfcbc7b617c16c80f2da5102d11fdc58858c446
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2023-08-16 14:24:45 +0000

    update account schemas

--- optuslib/schemas/dashboard/account.py
+++ optuslib/schemas/dashboard/account.py
@@ -1,9 +1,19 @@
 from pydantic import BaseModel
 
+from .charts import Chart
+from .indicators import Indicator, UsdLiquidity, UsdVolume
+
 
 class DashboardBaseAccount(BaseModel):
-    pass
+    id: int | None
+    address: str
+    name: str | None
+    description: str | None
+    liquidity: UsdLiquidity = UsdLiquidity()
+    volume_24h: UsdVolume = UsdVolume()
+    transactions_24h: Indicator = Indicator()
 
 
 class DashboardAccount(DashboardBaseAccount):
-    pass
+    liquidity_chart: Chart = Chart()
+    volume_chart: Chart = Chart()
