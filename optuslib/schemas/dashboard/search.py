8d7480b759d4971c35ba4779876a1505d8a1a140
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2023-09-15 17:04:25 +0000

    update DashboardSearch

--- optuslib/schemas/dashboard/search.py
+++ optuslib/schemas/dashboard/search.py
@@ -1,13 +1,17 @@
 from pydantic import BaseModel
 
-from .account import DashboardBaseAccount
-from .dex import DashboardBaseDex
-from .token import DashboardBaseToken
-from .pair import DashboardBasePair
+from .account import DashboardBaseAccount, DashboardAccount
+from .dex import DashboardBaseDex, DashboardDex
+from .token import DashboardBaseToken, DashboardToken
+from .pair import DashboardBasePair, DashboardPair
 
 
 class DashboardSearch(BaseModel):
-    account_list: list[DashboardBaseAccount] = []
+    dex: DashboardDex | None
+    token: DashboardToken | None
+    pair: DashboardPair | None
+    account: DashboardAccount | None
     dex_list: list[DashboardBaseDex] = []
     token_list: list[DashboardBaseToken] = []
     pair_list: list[DashboardBasePair] = []
+    account_list: list[DashboardBaseAccount] = []
