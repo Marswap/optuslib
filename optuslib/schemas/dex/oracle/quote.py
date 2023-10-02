6acea5f4c2788a132a917102153ac979e3edad72
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2023-10-02 11:28:26 +0000

    update Quote, QuoteRequest

--- optuslib/schemas/dex/oracle/quote.py
+++ optuslib/schemas/dex/oracle/quote.py
@@ -4,12 +4,12 @@ from ...base.base_models import BaseUserFriendlyAddressModel
 
 
 class QuoteRequest(BaseUserFriendlyAddressModel):
-    user_address: str = "EQC5aj2p6ClvTKx2-jWjSBrYbWTS_S2moHW4mRh2QOq1Um6x"
-    from_token_address: str = "EQDCJL0iQHofcBBvFBHdVG233Ri2V4kCNFgfRT-gqAd3Oc86"
-    to_token_address: str = "EQDQoc5M3Bh8eWFephi9bClhevelbZZvWhkqdo80XuY_0qXv"
-    from_jetton_wallet_address: str = "EQDgqKCh2UeenPS6g-jHnv_tbli1ilXDdkYHaBoVxKXBSaZD"
-    from_token_amount: int = 100000000000
-    slippage_tolerance: float = 0.1
+    user_address: str
+    from_token_address: str
+    to_token_address: str
+    from_token_amount: int | None
+    to_token_amount: int | None
+    slippage_tolerance: float
 
 
 class QuoteProtocol(BaseModel):
@@ -25,7 +25,6 @@ class QuoteMessage(BaseModel):
 
 
 class Quote(QuoteRequest):
-    to_token_amount: int
     price_impact: float
     protocols: list[QuoteProtocol]
     messages: list[QuoteMessage]
